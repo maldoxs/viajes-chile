139dd9b1477844ef89e0dd2923eac95ada32c11e
maldoxs <a.maldo23@gmail.com>
2021-08-10 23:47:40 -0400

    se modifica formulario

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -2,21 +2,21 @@
     Lo que hacemos es utilizar la función .animate() de jQuery 
     para crear una animación suave que durará 500 milisegundos
 */
-$(document).ready(function () {
-  $('a[href^="#"]').click(function () {
-    var destino = $(this.hash);
-    if (destino.length == 0) {
-      destino = $('a[name="' + this.hash.substr(1) + '"]');
-    }
-    if (destino.length == 0) {
-      destino = $("html");
-    }
-    $("html, body").animate(
-      {
-        scrollTop: destino.offset().top,
-      },
-      500
-    );
-    return false;
-  });
-});
+// $(document).ready(function () {
+//   $('a[href^="#"]').click(function () {
+//     var destino = $(this.hash);
+//     if (destino.length == 0) {
+//       destino = $('a[name="' + this.hash.substr(1) + '"]');
+//     }
+//     if (destino.length == 0) {
+//       destino = $("html");
+//     }
+//     $("html, body").animate(
+//       {
+//         scrollTop: destino.offset().top,
+//       },
+//       500
+//     );
+//     return false;
+//   });
+// });
